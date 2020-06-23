0c51509bf1de596691dd774126b61429ebe70dbc
Schrese <53787633+Schrese@users.noreply.github.com>
2020-06-23 14:54:24 -0400

    Finally got the class function to work

--- gol/src/components/game/Grid.js
+++ gol/src/components/game/Grid.js
@@ -1,75 +1,89 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-const grid = [
-    {id: 1, row: 1, col: 1 },
-    {id: 2, row: 1, col: 2 },
-    {id: 3, row: 1, col: 3},
-    {id: 4, row: 1, col: 4},
-    {id: 5, row: 1, col: 5},
+// const grid = [
+//     {id: 1, row: 1, col: 1 },
+//     {id: 2, row: 1, col: 2 },
+//     {id: 3, row: 1, col: 3},
+//     {id: 4, row: 1, col: 4},
+//     {id: 5, row: 1, col: 5},
 
-    {id: 6, row: 2, col: 1},
-    {id: 7, row: 2, col: 2},
-    {id: 8, row: 2, col: 3},
-    {id: 9, row: 2, col: 4},
-    {id: 10, row: 2, col: 5},
+//     {id: 6, row: 2, col: 1},
+//     {id: 7, row: 2, col: 2},
+//     {id: 8, row: 2, col: 3},
+//     {id: 9, row: 2, col: 4},
+//     {id: 10, row: 2, col: 5},
 
-    {id: 11, row: 3, col: 1},
-    {id: 12, row: 3, col: 2},
-    {id: 13, row: 3, col: 3},
-    {id: 14, row: 3, col: 4},
-    {id: 15, row: 3, col: 5},
+//     {id: 11, row: 3, col: 1},
+//     {id: 12, row: 3, col: 2},
+//     {id: 13, row: 3, col: 3},
+//     {id: 14, row: 3, col: 4},
+//     {id: 15, row: 3, col: 5},
 
-    {id: 16, row: 4, col: 1},
-    {id: 17, row: 4, col: 2},
-    {id: 18, row: 4, col: 3},
-    {id: 19, row: 4, col: 4},
-    {id: 20, row: 4, col: 5},
+//     {id: 16, row: 4, col: 1},
+//     {id: 17, row: 4, col: 2},
+//     {id: 18, row: 4, col: 3},
+//     {id: 19, row: 4, col: 4},
+//     {id: 20, row: 4, col: 5},
 
-    {id: 21, row: 5, col: 1},
-    {id: 22, row: 5, col: 2},
-    {id: 23, row: 5, col: 3},
-    {id: 24, row: 5, col: 4},
-    {id: 25, row: 5, col: 5},
+//     {id: 21, row: 5, col: 1},
+//     {id: 22, row: 5, col: 2},
+//     {id: 23, row: 5, col: 3},
+//     {id: 24, row: 5, col: 4},
+//     {id: 25, row: 5, col: 5},
 
-]
+// ]
+
+// Cell Class
+// has properties: id, row, col, alive, 
+class Cell {
+    constructor(id, row, col, isAlive) {
+        this.id = id
+        this.row = row
+        this.col = col
+        this.isAlive = isAlive
+    }
+}
+
 
 const Grid = () => {
     // const [currentGen, setCurrentGen] = useState([])
-    const [columns, setColumns] = useState(5)
-    const [rows, setRows] = useState(5)
+    // const [columns, setColumns] = useState(5)
+    // const [rows, setRows] = useState(5)
+    const [grid, setGrid] = useState([])
 
-    let gridMap = new Map()
+    // function to create rows/columns/cells
+    function generateCells(n) {
+        let gen_id = 0
+        let newCell
+        let arr = []
+        for (let i = 1; i <= n; i++){
 
-    // const generateGrid = (r, c) => {
-    //     for(let j = 0; j <= r; j++) {
-    //         console.log(j)
-    //         for(let i = 0; i <= c; i++){
-    //             // console.log(i, j)
-    //             // setCurrentGen(Array.from(Array(columns)))
-    //             console.log(i)
-    //             // gridMap.set(i+j, {j, i})
-    //             gridMap.set(j, i)
-    //         }
-    //     }
-    // // }
-    // generateGrid(rows, columns)
-    // console.log(gridMap)
+            for (let j = n; j > 0; j--) {
+                gen_id += 1
+                newCell = new Cell(gen_id, i, j, false)
+                arr.push(newCell)
+                // console.log(arr)
+                // setGrid(grid, newCell)
+                // console.log(newCell)
+                // return grid
+            }
+        }
+        return arr
+    }
 
-    // let obj = gridMap.entries();
-    // console.log(obj)
-    
-    console.log(grid)
+    let generated = generateCells(5)
+    console.log(generated, 'final product')
 
     return(
         <div className = 'secondary_container'>
             <h4>This Will be the Grid</h4>
                 <GridContainer className = 'grid_container'>
-                {grid.map(x => (
+                {/* {grid.map(x => (
                     <OneCell className = 'cell_container' key = {x.id}>
-                        {/* <p className = 'ind_cell'>Box</p> */}{x.id}
+                        <p className = 'ind_cell'>Box</p>{x.id}
                     </OneCell>
-                ))}
+                ))} */}
 
             </GridContainer>
         </div>
@@ -97,8 +111,8 @@ const GridContainer = styled.div`
     }
 `
 
-const OneCell = styled.div`
-    background: pink;
-    // width: 5rem;
-    height: 5rem;
-`
+// const OneCell = styled.div`
+//     background: pink;
+//     // width: 5rem;
+//     height: 5rem;
+// `
